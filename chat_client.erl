811c08f2659950f485628463be91f13f2037a118
Sean Liu <seanliugm@gmail.com>
2016-09-22 17:07:50 +0800

    removed RouterPid

--- chat_client.erl
+++ chat_client.erl
@@ -9,4 +9,4 @@ print_message(MessageBody) ->
   io:format("Received: ~p~n", [MessageBody]).
 
 start_router() ->
-  message_router:start(fun(MessageBody) -> chat_client:print_message(MessageBody) end).
+  message_router:start(fun chat_client:print_message/1).
