d0645f83d6479a59af457045707b17eb9a94a539
Sean Liu <seanliugm@gmail.com>
2016-09-23 17:58:34 +0800

    convert PrintFun to a process printing message

--- chat_client.erl
+++ chat_client.erl
@@ -5,11 +5,18 @@
 send_message(ClientName, MessageBody) ->
   message_router:send_chat_message(ClientName, MessageBody).
 
-print_message(Who, MessageBody) ->
-  io:format("~p received: ~p~n", [Who, MessageBody]).
-
 register_nickname(Nickname) ->
-  message_router:register_nick(Nickname, fun(MessageBody) -> print_message(Nickname, MessageBody) end).
+  Pid = spawn(chat_client, handle_messages, [Nickname]),
+  message_router:register_nick(Nickname, Pid).
+
+handle_messages(Nickname) ->
+  receive
+    {printmsg, MessageBody} ->
+      io:format("~p received: ~p ~n", [Nickname, MessageBody]),
+      handle_messages(Nickname);
+    stop ->
+      ok
+  end.
 
 unregister_nickname(Nickname) ->
   message_router:unregister_nick(Nickname).
